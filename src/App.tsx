265de44f0e215323a3223df07b178ebb8d676921
Dan Abel <dan@webmarmalade.co.uk>
2026-03-26 22:18:48 +0000

    add logos

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="">
+    <div className="bg-black">
       <div className="bg-black lg:bg-transparent pt-4 lg:py-4 px-6 lg:absolute flex flex-row">
         <img className="h-40" src={badgers} />
         <h1 className="ml-4 mt-3 sm:mt-4 text-3xl md:text-5xl text-white text-shadow-lg">
@@ -27,6 +27,15 @@ function App() {
       <div className="bg-black">
         <img className="ml-auto w-full" src={badger} />
       </div>
+      <div className="grid grid-cols-4 gap-4 items-center place-items-center m-6">
+        <a href="https://www.facebook.com/pontyclunrugbyclub/" target="_blank"><img className="rounded-xl" src="/pontyclun-rfc.jpeg" /></a>
+        <a href="https://www.juniordarts.com/" target="_blank"><img className="rounded-xl" src="/jdc.png" /></a>
+        <a href="https://www.reddragondarts.com/" target="_blank"><img className="" src="/red-dragon.jpeg" /></a>
+        <a href="https://winmau.com/" target="_blank"><img className="rounded-xl" src="/winmau.png" /></a>
+      </div>
+      <footer className="text-white text-center m-4">
+        &copy;2026 Pontyclun Badgers Junior Darts Academy 
+      </footer>
     </div>
   );
 }
